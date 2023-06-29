1a91511be74ca9e3f612944386fee2ac3dc1bce7
sponnet <stefaan@ponnet.com>
2023-06-29 14:19:57 +0200

    increase heap to 5Gig by default

--- build/reload-certs.sh
+++ build/reload-certs.sh
@@ -2,7 +2,7 @@
 while true; do
     date > /tmp/reload-certs.txt
 
-    echo "Check for updated certificates"
+    echo "Refreshing SSL certificates"
 
     md5sumbefore=$(md5sum "/opt/teku/my.ava.do.crt")
     curl "http://dappmanager.my.ava.do/my.ava.do.crt" --output /opt/teku/my.ava.do.crt --silent
@@ -14,6 +14,8 @@ while true; do
         keytool -delete -deststorepass avadoKeyStorePassword -destkeystore /opt/teku/avado.keystore -alias myavado
         keytool -importkeystore -deststorepass avadoKeyStorePassword -destkeystore /opt/teku/avado.keystore  -srckeystore /opt/teku/my.ava.do.p12 -srcstoretype PKCS12 -srcstorepass avadoKeyStorePassword -alias myavado
         supervisorctl restart teku
+    else
+        echo "No restart needed"
     fi
 
     #sleep one day
